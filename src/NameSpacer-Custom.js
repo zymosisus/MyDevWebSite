38b3f24c362dfa5ce531244a50be9a7ed31b1410
zymosisus <Development@TheDrew.org>
2012-07-05 21:28:07 -0400

    Inital Commit of working Weight Loss Tracker web page.

---
+++ src/NameSpacer-Custom.js
@@ -0,0 +1,19 @@
+﻿var THEDREWORG = THEDREWORG || {};
+(function (thedreworg) {
+	thedreworg.namespace = function (ns_string) {
+		var parts = ns_string.split('.')
+				, parent = THEDREWORG
+				, i;
+
+		if (parts[0] === "THEDREWORG")
+			parts = parts.slice(1);
+
+		for (i = 0; i < parts.length; i += 1) {
+			if (typeof parent[parts[i]] === "undefined") {
+				parent[parts[i]] = {};
+			}
+			parent = parent[parts[i]];
+		}
+		return parent;
+	};
+})(THEDREWORG);
---
+++ src/UsersRepository.js
@@ -0,0 +1,65 @@
+﻿THEDREWORG.namespace('userRepository');
+THEDREWORG.UserRepository = (function () {
+	return function () {
+		this.length = function () {
+            ///TODO: Unit test this method
+			var users = JSON.parse(localStorage.getItem('users'));
+			if (users) return users.length;
+			else return 0;
+		};
+
+		this.getAllUsers = function () {
+            ///TODO: Unit test this method
+			return JSON.parse(localStorage.getItem('users'));
+		};
+
+		this.getUserList = function () {
+			/// Returns a JSON of user names and user ids
+			var users = JSON.parse(localStorage.getItem('users')),
+				userLen = users.length, i, userList = new Array();
+			for (i = 0; i < userLen; i++) {
+				userList[i] = {};
+				userList[i].userIndex = i;
+				userList[i].userName = users[i].userName;
+			}
+			return userList;
+		};
+
+		this.getUserById = function (id) {
+            ///TODO: Unit test this method
+			var users = JSON.parse(localStorage.getItem('users'));
+			return users[id];
+		};
+
+		this.addUser = function (user) {
+            ///TODO: Unit test this method
+			var users = JSON.parse(localStorage.getItem('users')) || new Array()
+				, userIndex = this.length();
+			users[userIndex] = user;
+			localStorage.setItem('users', JSON.stringify(users));
+			return userIndex;
+		};
+
+		this.addUserWeighIn = function (userId, weighIn) {
+            ///TODO: Unit test this method
+			var users = JSON.parse(localStorage.getItem('users')) || new Array()
+				,user = users[userId];
+			user.weighIns[user.weighIns.length] = weighIn;
+			localStorage.setItem('users', JSON.stringify(users));
+		};
+
+		this.getUserWeighIns = function (userId) {
+            ///TODO: Unit test this method
+			var users = JSON.parse(localStorage.getItem('users'));
+			return users[userId].weighIns;
+		};
+
+		this.getCurrentWeighIn = function (userId) {
+            ///TODO: Unit test this method
+			var weighIns = this.getUserWeighIns(userId);
+			return weighIns[weighIns.length - 1];
+		};
+
+		//Delete Method goes here (Haven't gotten there yet)
+	};
+})();
